9167ea4842dd8af7c3cd0cbb568c9d143c31d2fd
is-hoku <this@is-hoku.info>
2022-01-01 17:30:29 +0900

    respond as user

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,43 @@
 import { App } from "@slack/bolt";
 import dotenv from "dotenv";
 
+dotenv.config();
+
 const app = new App({
 	token: process.env.SLACK_BOT_TOKEN,
 	signingSecret: process.env.SLACK_SIGNING_SECRET,
 });
 
-dotenv.config();
 const PORT = process.env.PORT || "3000";
+
+app.command("/clock_in", async ({ command, ack, respond, client }) => {
+	try {
+		await ack();
+
+		const user = await client.users.profile.get({
+			user: command.user_id,
+		});
+		if (user.profile == undefined || user.profile == null) {
+			throw new Error("Could not get user profile");
+		}
+		const text = command.text || "稼働します！";
+		const icon_url = user.profile!.image_original;
+		console.log(icon_url);
+		await client.chat.postMessage({
+			channel: command.channel_id,
+			text: text,
+			username: `${command.user_name}`,
+			icon_url: icon_url,
+		});
+	} catch (e: any) {
+		await respond({
+			text: `error: ${e.message}`,
+			response_type: "ephemeral",
+		});
+	}
+});
 
 (async () => {
 	await app.start(parseInt(PORT));
-
 	console.log("⚡️ Bolt app is running!");
 })();
