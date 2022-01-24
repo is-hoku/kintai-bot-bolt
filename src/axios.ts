e9d38bec8772b0158b5302a068c295121e7a5083
is-hoku <this@is-hoku.info>
2022-01-24 10:26:31 +0900

    add auth endpoint

--- src/axios.ts
+++ src/axios.ts
@@ -4,9 +4,16 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const baseURL = process.env.KINTAI_API_URL || "http://localhost:8080";
+const freeeURL = "https://api.freee.co.jp";
 
 export const AxiosClient: AxiosInstance = axios.create({
 	baseURL,
+	timeout: 3000,
+	headers: {},
+});
+
+export const AxiosFreeeClient: AxiosInstance = axios.create({
+	baseURL: freeeURL,
 	timeout: 3000,
 	headers: {},
 });
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import { App } from "@slack/bolt";
 import dotenv from "dotenv";
-import { AxiosClient } from "./axios";
+import { AxiosClient, AxiosFreeeClient } from "./axios";
 
 dotenv.config();
 
@@ -11,10 +11,36 @@ const app = new App({
 
 const PORT = process.env.PORT || "3000";
 
+app.command("/auth", async ({ command, ack, respond, client }) => {
+	try {
+		await ack();
+
+		const auth_url = await AxiosClient.get(`/auth`);
+		const res_url = auth_url.data.url || "Access Token is already set.";
+		await respond({
+			text: `認可URLにアクセスしてください。\n${res_url}`,
+			response_type: "ephemeral",
+		});
+	} catch (e: any) {
+		await respond({
+			text: `error: ${e.message}`,
+			response_type: "ephemeral",
+		});
+	}
+});
+
 app.command("/clock_in", async ({ command, ack, respond, client }) => {
 	try {
 		await ack();
 
+		// AccessToken のチェック
+		const companyID = process.env.COMPANY_ID || "0000000";
+		const token = await AxiosClient.get(`/token/${companyID}`);
+		if (token.data.AccessToken == "") {
+			throw new Error("Not Found Access Token");
+		}
+
+		// ユーザ ID とアイコン URL を取得
 		const user = await client.users.profile.get({
 			user: command.user_id,
 		});
@@ -24,8 +50,10 @@ app.command("/clock_in", async ({ command, ack, respond, client }) => {
 		const text = command.text || "稼働します！";
 		const icon_url = user.profile!.image_original;
 
+		// freee API を叩いて勤怠登録
 		const freeeID = await AxiosClient.get(`/user/${user.profile.email}`);
-		console.log(freeeID);
+		const users = await AxiosFreeeClient.get(`/users/me`);
+		console.log(users);
 		await client.chat.postMessage({
 			channel: command.channel_id,
 			text: text,
