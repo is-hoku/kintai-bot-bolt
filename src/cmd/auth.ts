292e57db2c2b7b09fa5fe3da70814454fc92bdf3
is-hoku <this@is-hoku.info>
2022-01-30 16:15:35 +0900

    fix token refresh process

--- src/cmd/auth.ts
+++ src/cmd/auth.ts
@@ -5,10 +5,10 @@ export const auth = async ({ ack, respond }: SlackCommandMiddlewareArgs) => {
 	try {
 		await ack();
 
-		const auth_url = await AxiosClient.get(`/auth`);
-		const res_url = auth_url.data.url || "Access Token is already set.";
+		const authURL = await AxiosClient.get(`/auth`);
+		const resURL = authURL.data.url || "Access Token is already set.";
 		await respond({
-			text: `認可URLにアクセスしてください。\n${res_url}`,
+			text: `認可URLにアクセスしてください。\n${resURL}`,
 			response_type: "ephemeral",
 		});
 	} catch (e: any) {
--- src/cmd/break_end.ts
+++ src/cmd/break_end.ts
@@ -10,21 +10,14 @@ export const break_end = async ({ command, ack, respond, client }: any) => {
 	try {
 		await ack();
 
-		// AccessToken のチェック
-		const companyID = process.env.COMPANY_ID || "0000000";
-		const token = await AxiosClient.get(`/token/${companyID}`);
-		if (
-			token.data.access_token == "" ||
-			token.data.access_token == undefined
-		) {
-			throw new Error("Not Found Access Token");
-		}
-
 		// AccessToken の Refresh
 		const refresh = await AxiosClient.get(`/refresh`);
 		if (refresh.status != 200) {
 			throw new Error("Failed to refresh access token");
 		}
+
+		const companyID = process.env.COMPANY_ID;
+		const accessToken = refresh.data.access_token;
 
 		// ユーザ ID とアイコン URL を取得
 		const user = await client.users.profile.get({
@@ -34,7 +27,7 @@ export const break_end = async ({ command, ack, respond, client }: any) => {
 			throw new Error("Could not get user profile");
 		}
 		const text = command.text || "再開します！";
-		const icon_url = user.profile!.image_original;
+		const iconURL = user.profile!.image_original;
 
 		// freee API を叩いて勤怠登録
 		const freeeID = await AxiosClient.get(`/user/${user.profile.email}`);
@@ -46,7 +39,7 @@ export const break_end = async ({ command, ack, respond, client }: any) => {
 			//datetime: new Date("2022-01-19T06:36:03.995Z"),
 		};
 		const config = {
-			headers: { Authorization: `Bearer ${token.data.access_token}` },
+			headers: { Authorization: `Bearer ${accessToken}` },
 		};
 		const result = await AxiosFreeeClient.post(
 			`/api/v1/employees/${freeeID.data.freee_id}/time_clocks`,
@@ -57,7 +50,7 @@ export const break_end = async ({ command, ack, respond, client }: any) => {
 			channel: command.channel_id,
 			text: text,
 			username: `${command.user_name}`,
-			icon_url: icon_url,
+			icon_url: iconURL,
 		});
 	} catch (e: any) {
 		await respond({
